dd49b67d9a1c44a7f18047a8ade8e0b29c19d7b2
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-27 12:13:03 +0100

    feat: Añadir caso test sell_in normal_item

--- src/test/java/edu/elsmancs/AppTest.java
+++ src/test/java/edu/elsmancs/AppTest.java
@@ -14,4 +14,13 @@ public class AppTest {
         assertEquals("10", item.getSell_in().toString());
         assertEquals("20", item.getQuality().toString());
     }
+
+    @Test
+    public void actualizarSetSell_in() {
+        Normal_item normal_item = new Normal_item("+5 Dexterity Vest", 10, 20);
+
+        assertEquals("+5 Dexterity Vest", normal_item.getName());
+        assertEquals("9", normal_item.getSell_in().toString());
+        assertEquals("20", normal_item.getQuality().toString());
+    }
 }
