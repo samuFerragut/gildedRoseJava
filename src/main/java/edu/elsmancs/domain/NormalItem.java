dcf2c4ee703663b105db06067825b6267b2e5633
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-28 11:59:43 +0100

    feat:Añadir logica update quality

--- src/main/java/edu/elsmancs/domain/NormalItem.java
+++ src/main/java/edu/elsmancs/domain/NormalItem.java
@@ -1,6 +1,13 @@
 package edu.elsmancs.domain;
 
 public class NormalItem {
+
+    /**
+     * Esta clase esta creada debido a que no
+     * se puede tocar la clase item.
+     * Su función es actualizar la quality
+     * y el sell_in de los objetos normales
+     */
     private final Item item;
 
     public NormalItem(String name, int sell_in, int quality) {
@@ -18,4 +25,27 @@ public class NormalItem {
     public int getQuality() {
         return item.getQuality();
     }
+
+    private void computeQuality(int value) {
+        if (getQuality() + value > 50) {
+            item.setQuality(50);
+        }
+        else if (getQuality() + value >= 0){
+            item.setQuality(getQuality() + value);
+        }
+        else {
+            item.setQuality(0);
+        }
+    }
+
+    public void updateQuality() {
+
+        if (getSell_in() > 0) {
+            computeQuality(-1);
+        } else {
+            computeQuality(-2);
+        }
+        item.setSell_in();
+    }
 }
+
