dd1bf363a21f309dbec60cf4b8005f8875619228
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-28 11:25:50 +0100

    feat:Añadir logica crear normalitem

--- src/main/java/edu/elsmancs/domain/NormalItem.java
+++ src/main/java/edu/elsmancs/domain/NormalItem.java
@@ -1,4 +1,21 @@
 package edu.elsmancs.domain;
 
 public class NormalItem {
+    private final Item item;
+
+    public NormalItem(String name, int sell_in, int quality) {
+        this.item = new Item(name, sell_in, quality);
+    }
+
+    public String getName() {
+        return item.getName();
+    }
+
+    public int getSell_in() {
+        return item.getSell_in();
+    }
+
+    public int getQuality() {
+        return item.getQuality();
+    }
 }
