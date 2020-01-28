9ed56f6cfee4f519daff6791ad2ffd69e6a2b85b
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-28 13:59:43 +0100

    feat:Añadir clase conjured

--- src/main/java/edu/elsmancs/domain/ConjuredItem.java
+++ src/main/java/edu/elsmancs/domain/ConjuredItem.java
@@ -1,4 +1,13 @@
 package edu.elsmancs.domain;
 
-public class ConjuredItem {
+public class ConjuredItem extends NormalItem{
+
+    /**
+     * Cuando el sell_in este a 0 o sea mayor a este
+     * se le resta 2 a la quality, si no se le resta 4
+     */
+
+    public ConjuredItem(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
 }
