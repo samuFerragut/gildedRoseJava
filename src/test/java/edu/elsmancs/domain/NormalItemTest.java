8f8d6e2c4f567159d8f4106b93638198a4bd6ca0
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-28 10:35:37 +0100

    Crear NormalItemTest con el primer caso test

--- src/test/java/edu/elsmancs/domain/NormalItemTest.java
+++ src/test/java/edu/elsmancs/domain/NormalItemTest.java
@@ -1,4 +1,18 @@
 package edu.elsmancs.domain;
 
+import static org.junit.Assert.*;
+
+import edu.elsmancs.domain.NormalItem;
+import org.junit.Test;
+
 public class NormalItemTest {
+
+    @Test
+    public void crearNormalItem(){
+
+        NormalItem normal = new NormalItem(name: "+5 Dexterity Vest", sell_in: 10, quality: 20);
+        assertEquals("+5 Dexterity Vest", normal.getName());
+        assertEquals(10, normal.getSell_in(), 0);
+        assertEquals(20, norml.getQuality(), 0);
+    }
 }
