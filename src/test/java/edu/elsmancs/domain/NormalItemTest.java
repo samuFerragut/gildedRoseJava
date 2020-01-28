2c465e56ae0f42df79d144db076da562fb9fcd00
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-28 11:30:54 +0100

    feat:Añadir caso test toString

--- src/test/java/edu/elsmancs/domain/NormalItemTest.java
+++ src/test/java/edu/elsmancs/domain/NormalItemTest.java
@@ -10,9 +10,15 @@ public class NormalItemTest {
     @Test
     public void crearNormalItem(){
 
-        NormalItem normal = new NormalItem(name: "+5 Dexterity Vest", sell_in: 10, quality: 20);
+        NormalItem normal = new NormalItem("+5 Dexterity Vest",10,20);
         assertEquals("+5 Dexterity Vest", normal.getName());
         assertEquals(10, normal.getSell_in(), 0);
-        assertEquals(20, norml.getQuality(), 0);
+        assertEquals(20, normal.getQuality(), 0);
+    }
+
+    @Test
+    public void toStringTest() {
+        NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+        System.out.println(normal.toString());
     }
 }
