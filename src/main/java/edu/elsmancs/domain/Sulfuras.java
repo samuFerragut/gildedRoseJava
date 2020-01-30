41b214ce541c299dcf66ada3c5b3d5ce595bd137
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-30 16:24:37 +0100

    feat:Añadir clase Sulfuras + constructor

---
+++ src/main/java/edu/elsmancs/domain/Sulfuras.java
@@ -0,0 +1,8 @@
+package edu.elsmancs.domain;
+
+public class Sulfuras extends NormalItem {
+
+    public Sulfuras(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+}
