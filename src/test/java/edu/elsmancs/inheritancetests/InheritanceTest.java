4231f4e76cea68af2d2acf4f6547499adafde66b
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-30 16:12:44 +0100

    test:Añadir tests AgedBrie a InheritanceTest

--- src/test/java/edu/elsmancs/inheritancetests/InheritanceTest.java
+++ src/test/java/edu/elsmancs/inheritancetests/InheritanceTest.java
@@ -1,9 +1,12 @@
 package edu.elsmancs.inheritancetests;
 
 import static org.junit.Assert.*;
+
+import edu.elsmancs.domain.AgedBrie;
 import org.junit.Test;
 
 import edu.elsmancs.domain.NormalItem;
+import edu.elsmancs.domain.AgedBrie;
 
 public class InheritanceTest {
 
@@ -44,5 +47,50 @@ public class InheritanceTest {
         normal.updateQuality();
         assertEquals(9, normal.getSell_in(), 0);
         assertEquals(0, normal.getQuality(), 0);
+    }
+    @Test
+    public void crearAgedBrie() {
+
+        AgedBrie cheese = new AgedBrie("Aged Brie", 10, 20);
+        assertEquals("Aged Brie", cheese.getName());
+        assertEquals(10, cheese.getSell_in(), 0);
+        assertEquals(20, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void toStringTestAged() {
+        AgedBrie cheese = new AgedBrie("Aged Brie", 10, 20);
+        System.out.println(cheese.toString());
+    }
+
+    @Test
+    public void updateQualityAgedBrie() {
+        AgedBrie cheese = new AgedBrie("Aged Brie", 2, 0);
+        cheese.updateQuality();
+        assertEquals(1, cheese.getSell_in(), 0);
+        assertEquals(1, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void updateQualityAgedBrieSellInCERO() {
+        AgedBrie cheese = new AgedBrie("Aged Brie", 0, 0);
+        cheese.updateQuality();
+        assertEquals(-1, cheese.getSell_in(), 0);
+        assertEquals(2, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void updateQualityAgedBrieCERO() {
+        AgedBrie cheese = new AgedBrie("Aged Brie", 0, 0);
+        cheese.updateQuality();
+        assertEquals(-1, cheese.getSell_in(), 0);
+        assertEquals(2, cheese.getQuality(), 0);
+    }
+    @Test
+    public void updateQualityAgedBrieTerminaCERO() {
+        AgedBrie cheese = new AgedBrie("Aged Brie", 1, 0);
+        cheese.updateQuality();
+        assertEquals(0, cheese.getSell_in(), 0);
+        assertEquals(1, cheese.getQuality(), 0);
     }
 }
