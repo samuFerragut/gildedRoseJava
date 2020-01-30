89e5b86c801ffd571034c49a8ded079ede7aa419
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-30 16:01:53 +0100

    feat: Añadir logica sell_in + quality

--- src/main/java/edu/elsmancs/domain/AgedBrie.java
+++ src/main/java/edu/elsmancs/domain/AgedBrie.java
@@ -1,4 +1,21 @@
 package edu.elsmancs.domain;
 
-public class AgedBrie {
+
+public class AgedBrie extends NormalItem {
+
+    public AgedBrie(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+
+        if (getSell_in() > 0) {
+            computeQuality(1);
+        } else {
+            computeQuality(2);
+        }
+        setSell_in();
+    }
+
 }
