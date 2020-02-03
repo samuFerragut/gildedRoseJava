eae9e93470871e34f0c9111b76399581e4c09240
samuFerragut <sferragut@cifpfbmoll.eu>
2020-02-03 12:06:22 +0100

    feat(Conjured, ConjuredTest): Crear clase Conjured + fichero casos test

---
+++ src/main/java/edu/elsmancs/domain/Conjured.java
@@ -0,0 +1,7 @@
+package edu.elsmancs.domain;
+
+public class Conjured extends NormalItem {
+    public Conjured(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+}
