4eb2f4704fa0d68aad57e0de608551d3b9265f94
samuFerragut <sferragut@cifpfbmoll.eu>
2020-02-03 12:04:32 +0100

    feat(Backstage, BackstageTest): Crear clase Backstage + fichero test

---
+++ src/main/java/edu/elsmancs/domain/Backstage.java
@@ -0,0 +1,7 @@
+package edu.elsmancs.domain;
+
+public class Backstage extends NormalItem {
+    public Backstage(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+}
